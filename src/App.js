70bf73e4b3d03450a8f833c541a6812a06a62867
Mateusz <starszy1994@gmail.com>
2023-02-07 19:03:37 +0100

    Initial version v3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 const App = () => {
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+      <Routes>
+
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
